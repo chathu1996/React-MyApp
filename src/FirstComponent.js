c84746695d7e091863f14aa424efd2d2f4043195
chathu1996 <galahitiyawa@gmail.com>
2021-11-06 12:16:23 +0530

    Nesting Components

---
+++ src/FirstComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class FirstComponents extends Component{
+    render() {
+        return(
+            <div className="first-component">
+                <div>Name: Chathu</div>
+                <div>Age: 25</div>
+                <div>University: Sabaragamuwa University</div>
+            </div>
+        ); 
+    }
+}
+
+export default FirstComponents;
