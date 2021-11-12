3240cc8ad11a07f9d13f41178be923d540e8ba5e
chathu1996 <galahitiyawa@gmail.com>
2021-11-12 15:45:32 +0530

    deleting data

--- src/FirstComponent.js
+++ src/FirstComponent.js
@@ -1,37 +1,21 @@
 import React from "react";
 
-const FirstComponents = ( {students}) =>{
-    // const studentList = students.map(student =>{
-    //     if (student.age > 20) {
-    //         return(
-    //         <div className="first-component" key={student.id}>
-    //             <div>Name: { student.name }</div>
-    //             <div>Age: { student.age }</div>
-    //             <div>Belt: { student.belt }</div>
-    //             <br />
-    //             </div>
-    //         )
-    //     } else {
-    //         return null;
-    //     }
-    //  });
- 
-    return(
-        <div className="student-list">
-           { 
-            students.map( student => {
-                    return student.age > 20 ? (
-                        <div className="first-component" key={student.id}>
-                            <div>Name: { student.name }</div>
-                            <div>Age: { student.age }</div>
-                            <div>Belt: { student.belt }</div>
-                            <br />
-                        </div> 
-                    ) : null;
-                }) 
-            } 
-        </div>
-     ); 
+const FirstComponents = ( {students, deleteStudent }) =>{
+    const studentList = students.map(student =>{
+        if (student.age > 20) {
+            return(
+            <div className="first-component" key={student.id}>
+                <div>Name: { student.name }</div>
+                <div>Age: { student.age }</div>
+                <div>Belt: { student.belt }</div>
+                <button onClick={ deleteStudent( student.id) }>Delete Student</button>
+                <br />
+                </div>
+            )
+        } else {
+            return null;
+        }
+     });
 }
 
 export default FirstComponents;
